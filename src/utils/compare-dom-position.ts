e722cf92703291133fbea6e02d0806c46f413f1f
Jackson Hardaker <jackson.hardaker@gmail.com>
2020-08-11 21:38:11 -0400

    chore: remove util

--- src/utils/compare-dom-position.ts
+++ src/utils/compare-dom-position.ts
@@ -1,17 +1,13 @@
-const orAndCombined = (a: number, b: number) => [a, b, a | b]
+const isAfter = (position: number): Boolean => [
+  Node.DOCUMENT_POSITION_PRECEDING,
+  Node.DOCUMENT_POSITION_CONTAINS,
+  Node.DOCUMENT_POSITION_PRECEDING | Node.DOCUMENT_POSITION_CONTAINS,
+].includes(position)
 
-const isAfter = (position: number): Boolean => {
-  return orAndCombined(
-    Node.DOCUMENT_POSITION_PRECEDING,
-    Node.DOCUMENT_POSITION_CONTAINS)
-    .includes(position)
-}
-
-const isBefore = (position: number): Boolean => {
-  return orAndCombined(
-    Node.DOCUMENT_POSITION_FOLLOWING,
-    Node.DOCUMENT_POSITION_CONTAINED_BY)
-    .includes(position)
-}
+const isBefore = (position: number): Boolean => [
+  Node.DOCUMENT_POSITION_FOLLOWING,
+  Node.DOCUMENT_POSITION_CONTAINED_BY,
+  Node.DOCUMENT_POSITION_FOLLOWING | Node.DOCUMENT_POSITION_CONTAINED_BY
+].includes(position)
 
 export const evalPosition = (position: number): number => isAfter(position) ? 1 : isBefore(position) ? -1 : 0
